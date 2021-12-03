64ffbdb16516866b6db6ad47c9280d7ef1a29bc2
Ben Snyder <ben@snyder.tech>
2021-12-03 18:55:22 -0500

    ng update @angular/material@8

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import {
-MatButtonModule,
-MatCardModule,
-MatInputModule,
-MatBadgeModule
-} from '@angular/material';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
 import { UserIdleModule } from 'angular-user-idle';
 
 import { AppComponent } from './app.component';
